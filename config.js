37edf9fb8e1fd362ff1b1214b6ce258289da0d94
estonie568 <110975514+estonie568@users.noreply.github.com>
2023-04-14 19:41:34 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 exports.Prefix = `>`;//your prefix for bot
-exports.Token = `MTAxNzA1NTczNDQ0MzI5NDgwMg.G6JX68.5yiza6WRzqJ9l2x6WirN1U6ayS8N3NIdcXzRcA`;//your token 
+exports.Token = `TOKEN`;//your token 
 exports.Color = `BLUE`;//color   of embed
